e95313209f88c0e1868c7176ec32fa35a353f577
Ana Júlia <anajcaetanom@gmail.com>
2026-08-20 14:45:26 -0300

    definindo as coisas de types.h

--- xdp_ddos_protection.c
+++ xdp_ddos_protection.c
@@ -1,11 +1,48 @@
+typedef unsigned char       __u8;
+typedef unsigned short      __u16;
+typedef unsigned int        __u32;
+typedef unsigned long long  __u64;
+
+typedef signed char         __s8;
+typedef signed short        __s16;
+typedef signed int          __s32;
+typedef signed long long    __s64;
+
 #include <linux/bpf.h>
 #include <bpf/bpf_helpers.h>
 #include <bpf/bpf_endian.h>
-#include <linux/if_ether.h>
-#include <linux/ip.h>
+// #include <linux/if_ether.h>
+// #include <linux/ip.h>
 
-#define IPPROTO_ICMP 1
-#define ICMP_ECHO 8
+#define ETH_P_IP      0x0800 
+#define IPPROTO_ICMP  1
+#define ICMP_ECHO     8
+
+struct ethhdr {
+    __u8  h_dest[6];
+    __u8  h_source[6];
+    __u16 h_proto;
+};
+
+struct iphdr {
+#if __BYTE_ORDER__==__ORDER_LITTLE_ENDIAN__
+    __u8 ihl:4;
+    __u8 version:4;
+#else
+    __u8 version:4;
+    __u8 ihl:4;
+#endif
+
+    __u8 tos;
+    __u16 tot_len;
+    __u16 id;
+    __u16 frag_off;
+    __u8  ttl;
+    __u8  protocol;
+    __u16 check;
+    __u32 saddr;
+    __u32 daddr;
+};
 
 struct icmphdr {
     __u8 type;
@@ -14,6 +51,12 @@ struct icmphdr {
 };
 
 #define RATE_LIMIT 3
+
+#define XDP_ABORTED  0
+#define XDP_DROP     1
+#define XDP_PASS     2
+#define XDP_TX       3
+#define XDP_REDIRECT 4
 
 struct rate_limit_entry {
     __u64 last_update;
@@ -33,6 +76,7 @@ struct {
     __type(key, __u32);
     __type(value, __u8);
 } blacklist_map SEC(".maps");
+
 SEC("xdp")
 int xdp_prog(struct xdp_md *ctx)
 {
@@ -66,7 +110,7 @@ int xdp_prog(struct xdp_md *ctx)
         return XDP_PASS;
 
     // Apenas ping request
-if (icmp->type != ICMP_ECHO)
+    if (icmp->type != ICMP_ECHO)
         return XDP_PASS;
 
     __u32 src_ip = ip->saddr;
