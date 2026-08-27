d078cc5f9b88bb2b3a4769b6b4c8ff91af8ac6d1
Ana Júlia <anajcaetanom@gmail.com>
2026-08-27 13:06:11 -0300

    typedefs

--- pin.c
+++ pin.c
@@ -1,3 +1,13 @@
+typedef unsigned char       __u8;
+typedef unsigned short      __u16;
+typedef unsigned int        __u32;
+typedef unsigned long long  __u64;
+
+typedef signed char         __s8;
+typedef signed short        __s16;
+typedef signed int          __s32;
+typedef signed long long    __s64;
+
 #include <bpf/libbpf.h>
 #include <bpf/bpf.h>
 #include <stdio.h>
