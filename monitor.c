a686812cf5d27ba2be00231710700e02329c4af0
Ana Júlia <anajcaetanom@gmail.com>
2026-08-27 12:34:27 -0300

    compatibilidade

--- monitor.c
+++ monitor.c
@@ -1,3 +1,13 @@
+typedef unsigned char       __u8;
+typedef unsigned short      __u16;
+typedef unsigned int        __u32;
+typedef unsigned long long  __u64;
+
+typedef signed char         __s8;
+typedef signed short        __s16;
+typedef signed int          __s32;
+typedef signed long long    __s64;
+
 #include <stdio.h>
 #include <unistd.h>
 #include <arpa/inet.h>
